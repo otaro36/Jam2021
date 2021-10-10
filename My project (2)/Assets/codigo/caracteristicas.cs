ffabdbd099fe6c119c8979af0a6eff440e097371
WD2M <92236766+WD2M@users.noreply.github.com>
2021-10-10 13:04:24 -0500

    solu error

---
+++ My project (2)/Assets/codigo/caracteristicas.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class caracteristicas : MonoBehaviour
+{
+    public float filo;
+    public float dureza;
+    public float resistencia;
+}
